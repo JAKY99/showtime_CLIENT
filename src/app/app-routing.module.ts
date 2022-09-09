3fc008ed8ac3cc442d3a4166c559fa5b1d3166ac
Achot BARSEGHYAN <achot.barseghyan@gmail.com>
2022-09-09 20:21:48 +0200

    feat: restore config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
       {
         path: '',
         component: HomePageComponent,
-        //canActivate: [AuthGuard],
+        canActivate: [AuthGuard],
       }
     ]
   },
