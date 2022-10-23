0b0441efe347d9226c8b5677dca4d4c072558efa
Yvan Darmet <cmoiyvan@gmail.com>
2022-10-24 00:02:39 +0200

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -55,8 +55,6 @@ import { GoBackComponent } from './components/go-back/go-back.component';
 import {TabMenuModule} from "primeng/tabmenu";
 import { TvDetailsPageComponent } from './pages/tv-details-page/tv-details-page.component';
 import { TruncatedDescriptionComponent } from './components/truncated-description/truncated-description.component';
-import { TvDetailsPageComponent } from './pages/tv-details-page/tv-details-page.component';
-import { TruncatedDescriptionComponent } from './components/truncated-description/truncated-description.component';
 import { DialogModule } from 'primeng/dialog';
 import { LazyImgDirective } from './lazy-img.directive';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
