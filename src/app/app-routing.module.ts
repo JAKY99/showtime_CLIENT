4e6c111c78983632058f2a9d168a5d46a3626eb7
kevP-Sirius <kev972@gmail.com>
2023-03-28 20:29:43 +0200

    maj privacy policy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,12 +13,14 @@ import {TvDetailsComponent} from "./components/tv-details/tv-details.component";
 import{RegisterPageComponent} from "./pages/register-page/register-page.component";
 import { ChangePasswordPageComponent } from './pages/change-password-page/change-password-page.component';
 import {MainSearchPageComponent} from "./pages/main-search-page/main-search-page.component";
+import {PrivacyPageComponent} from "./pages/privacy-page/privacy-page.component";
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'login', component: LoginPageComponent},
   {path: 'reset', component: ResetPageComponent},
   {path: 'register', component: RegisterPageComponent},
   {path: 'reset-password/:token', component: ChangePasswordPageComponent},
+  {path:'privacy', component: PrivacyPageComponent},
   {
     path: 'home',
     children:[
@@ -53,7 +55,7 @@ const routes: Routes = [
     path:'search',
     component: MainSearchPageComponent,
     canActivate: [AuthGuard]
-  },
+  }
 ];
 
 @NgModule({
