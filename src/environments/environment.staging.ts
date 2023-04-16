d7e994957cc975390217a8209ca293e8fd92f99a
kevP-Sirius <kev972@gmail.com>
2023-04-16 22:56:17 +0200

    maj login redirection mode

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://dev.showtime-app.click/api',
   webSocketUrl: 'https://dev.showtime-app.click/api/websocket',
-  env: 'dev'
+  env: 'dev',
+  google_login_uri: 'https://dev.showtime-app.click/api/api/v1/login/google',
 };
