560ba2ac3b59687655f38acb6094ea40ceeeada3
kevP-Sirius <kev972@gmail.com>
2023-04-18 13:19:33 +0200

    maj logout function

--- src/app/services/token-storage.service.ts
+++ src/app/services/token-storage.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {JwtHelperService} from "@auth0/angular-jwt";
 import {RolesEnum} from "../common/enums/authorities/roles-enum";
-import {UserService} from "../services/user/user.service";
 const TOKEN_KEY = 'auth-token';
 
 @Injectable({
