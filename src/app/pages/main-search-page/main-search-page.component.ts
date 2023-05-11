b88da679ef288d04bdcacc5a8e6c57c61885ae4a
Yvan Darmet <cmoiyvan@gmail.com>
2023-05-11 15:58:21 +0200

    clic genre > redirect vers search + display by genre

--- src/app/pages/main-search-page/main-search-page.component.ts
+++ src/app/pages/main-search-page/main-search-page.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {TrendingService} from "../../services/trending/trending.service";
 import {MainSearchComponent} from "../../components/search/main-search/main-search.component";
 import {MediaDetailsDialogComponent} from "../../components/media-details-dialog/media-details-dialog.component";
+import {ActivatedRoute} from "@angular/router";
+import {TvService} from "../../services/tv/tv.service";
 
 @Component({
   selector: 'app-main-search-page',
@@ -18,17 +20,43 @@ export class MainSearchPageComponent implements OnInit {
   trendingResults: [] = [];
 
   totalResults: number = 0;
+  displayResult:number = 0;
 
   isLoading: boolean = false;
   isLoadMoreAvailable: boolean = true;
+  param : number | undefined;
 
-  constructor(private trendingService: TrendingService) { }
+  constructor(
+    private trendingService: TrendingService ,
+    private route: ActivatedRoute ,
+    private tvService : TvService
+  ) {}
 
   ngOnInit(): void {
-    this.trendingService.fetchAllTrendings().subscribe(resp => {
-      resp = JSON.parse(resp.data);
-      this.trendingResults = resp.results;
-    });
+
+    this.route.queryParams
+      .subscribe(params => {
+        this.param = params.genre
+        }
+      );
+
+    if(!this.param){
+      this.trendingService.fetchAllTrendings().subscribe(resp => {
+        resp = JSON.parse(resp.data);
+        this.trendingResults = resp.results;
+      });
+    }else{
+      // @ts-ignore
+      this.tvService.fetchListByGenre(this.param).subscribe(resp => {
+        resp = JSON.parse(resp.data);
+        console.log(resp)
+        this.trendingResults = resp.results;
+        this.displayResult = 1;
+        this.totalResults = resp.total_results
+      });
+    }
+
+
   }
 
   getMainSearchResults($event: any){
@@ -37,6 +65,7 @@ export class MainSearchPageComponent implements OnInit {
     this.isLoadMoreAvailable = true;
     this.mainSearchResults = $event.results;
     this.totalResults = $event.total_results;
+    this.displayResult = 2
     if($event.page === $event.total_pages){
       this.isLoadMoreAvailable = false;
     }
